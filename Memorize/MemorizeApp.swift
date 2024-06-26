54569d1ad4b9e9bb9230870da02353e04ccbd2c0
Edson Canganjo <edsoncanganjo@hotmail.com>
2024-06-26 23:17:30 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Edson Canganjo on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
